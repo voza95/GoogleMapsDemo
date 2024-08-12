014dc45804d2e6b2ded7de763a2d95780e5f40e2
Vaibhav Oza <vaibhavoza918@gmail.com>
2024-08-12 22:51:37 +0530

    Initial commit

---
+++ app/src/main/java/com/oza/googlemapsdemo/MapsActivity.kt
@@ -0,0 +1,108 @@
+package com.oza.googlemapsdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.lifecycleScope
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.oza.googlemapsdemo.databinding.ActivityMapsBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, MenuProvider {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        addMenuProvider(this)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15F))
+        mMap.uiSettings.apply {
+            isZoomControlsEnabled = true
+//            isZoomGesturesEnabled = truer
+//            isScrollGesturesEnabled = false
+            isMyLocationButtonEnabled = true
+        }
+
+        val losAngles: CameraPosition = CameraPosition.Builder()
+            .target(LatLng(34.052235, -118.243683))
+            .zoom(17F)
+            .bearing(100F)
+            .tilt(45F)
+            .build()
+        lifecycleScope.launch {
+            delay(4000L)
+//            mMap.animateCamera(CameraUpdateFactory.zoomTo(15F), 2000, null)
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(losAngles), 2000, object: GoogleMap.CancelableCallback {
+                override fun onCancel() {
+                    Toast.makeText(this@MapsActivity, "Cancelled", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onFinish() {
+                    Toast.makeText(this@MapsActivity, "Finished", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }
+    }
+
+
+    override fun onDestroy() {
+        removeMenuProvider(this)
+        super.onDestroy()
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.map_type_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        when(menuItem.itemId) {
+            R.id.normal_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            }
+            R.id.hybrid_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+            }
+            R.id.satellite_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            }
+            R.id.terrain_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            }
+            R.id.none_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_NONE
+            }
+        }
+        return true
+    }
+}
